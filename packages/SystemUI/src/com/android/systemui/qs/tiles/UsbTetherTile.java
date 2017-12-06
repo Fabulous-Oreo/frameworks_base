90ad2f71b14564ecff0e784f021fe8b2a805ae49
Adrian DC <radian.dc@gmail.com>
2017-12-06 07:33:04 +0100

    Tiles: setListening -> handleSetListening
    Change-Id: I35dd400ca1de286706db1ed55762a393ad9c142a

--- packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
@@ -55,7 +55,7 @@ public class UsbTetherTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public void setListening(boolean listening) {
+    public void handleSetListening(boolean listening) {
         if (mListening == listening)
             return;
         mListening = listening;
